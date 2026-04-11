3804a4d8d1364702fc98046beccd1e15d01b6b10
Prince <kittipop.suwanna@gmail.com>
2026-04-12 00:39:13 +0700

    feat : get reviews with query and pahination

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -12,20 +12,49 @@ const Company = require('../models/Company');
  * @route   GET /api/v1/reviews
  * @access  Public
  */
-exports.getReviews = async (req, res, next) => {
-  try {
-    let query;
+exports.getReviews = async (req, res, next) => { 
+  let query;
+  const reqQuery = { ...req.query };
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+  removeFields.forEach(param => delete reqQuery[param]);
 
+  let queryStr = JSON.stringify(reqQuery);
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  //pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  
+
+  try {
     if (req.params.id) {
-      query = Review.find({ company: req.params.id })
+      queryStr.company = req.params.id
+      query = Review.find(JSON.parse(queryStr))
         .populate({ path: 'user', select: 'name email' });
     } else {
-      query = Review.find()
+      query = Review.find(JSON.parse(queryStr))
         .populate({ path: 'company', select: 'name' })
         .populate({ path: 'user', select: 'name email' });
     }
-
+    
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+    
+    const total = await Review.countDocuments();
+    query = query.skip(startIndex).limit(limit);
     const reviews = await query;
+
+    
+    const pagination = {};
+    if (endIndex < total) pagination.next = { page: page + 1, limit };
+    if (startIndex > 0) pagination.prev = { page: page - 1, limit };
+
     res.status(200).json({ success: true, count: reviews.length, data: reviews });
   } catch (err) {
     console.log(err);
