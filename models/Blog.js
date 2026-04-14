bb6616f6ce51c8fbabfe00cab22c7a944483dd71
Piriyakorn Suphakitcharoen <tango271edu@gmail.com>
2026-04-15 01:50:00 +0700

    Add Blog & Comment model

---
+++ models/Blog.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const BlogSchema = new mongoose.Schema({
+    title: { type: String, required: [true, 'Please add a title'], trim: true, minlength: [1, 'Please add a title'], maxlength: [50, 'Title can not be more than 50 characters'] },
+    content: { type: String, trim: true, maxlength: [250, 'Body can not be more than 50 characters'] },
+    author: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+    numComments: { type: Number, default: 0 },
+    isDeletedByAdmin: {type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now },
+    edited : {type : Boolean, default : false},
+    editedAt: { type: Date, default: Date.now },
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+})
+
+BlogSchema.virtual('comments', {
+  ref: 'Comment',
+  localField: '_id',
+  foreignField: 'blog',
+  justOne: false,
+});
+
+module.exports = mongoose.model('Blog', BlogSchema);
---
+++ models/Comment.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+    text: { type: String, required: [true, "Please add a comment text"], trim: true, minlength: [1, 'Please add a comment text'], maxlength: [100, 'Comment cannot exceed 100 characters'] },
+    blog: { type: mongoose.Schema.ObjectId, ref: 'Blog', required: true },
+    author: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+    createdAt: { type: Date, default: Date.now },
+    edited : {type : Boolean, default : false },
+    editedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
